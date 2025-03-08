4979ce4ba3e225dea48a2c9afcdb2106a9abef45
Dhahbi Fadi <netflate@gmail.com>
2025-03-08 23:11:54 +0100

    directories structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
-#include <QApplication>
-#include "ui/windows/MainWindow.h"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QDir>
+#include "ui/models/FileSystemModel.h"
 
-int main(int argc, char *argv[]) {
-  QApplication app(argc, argv);
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
 
-  MainWindow mainWindow;
-  mainWindow.show();
+    app.setOrganizationName("YourOrganization");
+    app.setApplicationName("FileManager");
 
-  return app.exec();
+    QQmlApplicationEngine engine;
+
+    // Set the import paths explicitly
+    engine.addImportPath("qrc:/");
+
+    // Optional: Print all import paths for debugging
+    qDebug() << "QML import paths:" << engine.importPathList();
+
+    qmlRegisterType<FileSystemModel>("FileManager", 1, 0, "FileSystemModel");
+    FileSystemModel *fileSystemModel = new FileSystemModel();
+    engine.rootContext()->setContextProperty("fileSystemModel", fileSystemModel);
+
+    engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
+
+    if (engine.rootObjects().isEmpty()) {
+        return -1;
+    }
+
+    return app.exec();
 }
--- src/ui/windows/SettingsDialog.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by netfl on 28.02.2025.
-//
-
-#include "SettingsDialog.h"
